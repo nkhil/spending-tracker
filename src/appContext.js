c46c43adf32d838aeeba399605db2aacf8a94382
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2020-07-24 22:38:19 +0100

    🔥 delete unnecessary actions

--- src/appContext.js
+++ src/appContext.js
@@ -1,45 +1,26 @@
 import React, { createContext, useContext, useReducer } from 'react';
 import actions from './actions';
-import { addProductToBasket, deductProductFromBasket } from './helpers/basketHelper';
 import { addTransactionToState } from './helpers/addTransactionToState';
 
 const AppContext = createContext();
 
 const appStateReducer = (state, action) => {
   switch (action.type) {
-  case actions.ADD_TRANSACTION: {
-    // Attempt to add to db
-    const newTrxState = addTransactionToState(state.transactions, action.transaction);
-    return {
-      ...state,
-      transactions: newTrxState,
-    };
-  }
-  case 'ADD_TO_BASKET':
-  case 'INCREMENT_ITEM': {
-    const newBasketState = addProductToBasket(state.basket, action.transaction);
-    localStorage.setItem('basket', JSON.stringify(newBasketState));
-    return {
-      ...state,
-      basket: newBasketState,
-    };
-  }
-  case 'GET_FROM_LOCAL_STATE': {
-    return {
-      ...state,
-      basket: action.basket,
-    };
-  }
-  case 'DECREMENT_ITEM': {
-    const newBasketState = deductProductFromBasket(state.basket, action.product);
-    localStorage.setItem('basket', JSON.stringify(newBasketState));
-    return {
-      ...state,
-      basket: newBasketState,
-    };
-  }
-  default:
-    return state;
+    case actions.ADD_TRANSACTION: {
+      const newTrxState = addTransactionToState(state.transactions, action.transaction);
+      return {
+        ...state,
+        transactions: newTrxState,
+      };
+    }
+    case actions.ADD_TRANSACTIONS: {
+      return {
+        ...state,
+        transactions: action.transactions,
+      };
+    }
+    default:
+      return state;
   }
 };
 
